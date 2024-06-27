793cffcfede23ad04a44bd0ef02849afaf8859ff
siwoo <power12562@gmail.com>
2024-06-27 09:42:43 +0900

    csv 파싱 추가

--- D2DEngine/Sourece/Core/Component/SpriteAnimation.cpp
+++ D2DEngine/Sourece/Core/Component/SpriteAnimation.cpp
@@ -3,6 +3,10 @@
 #include "Framework/TimeSystem.h"
 #include "Core/GameObject/GameObjectBase.h"
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
 std::map<std::wstring, SpriteAnimation::AnimationClip*> SpriteAnimation::clipResourceMap;
 
 SpriteAnimation::SpriteAnimation(GameObjectBase& gameObject) : ComponentBase(gameObject)
@@ -20,7 +24,11 @@ void SpriteAnimation::LoadAnimationClip(const wchar_t* path, const wchar_t* clip
 	auto iter = Animations.find(clipName);
 	if (iter == Animations.end())
 	{
-		Animations[clipName] = std::pair<std::wstring, AnimationClip*>(path, CreateAnimationClipFromFile(path));
+		AnimationClip* newClip = CreateAnimationClipFromFile(path);
+		if (newClip)
+		{
+			Animations[clipName] = std::pair<std::wstring, AnimationClip*>(path, newClip);
+		}
 	}
 	else
 	{
@@ -133,12 +141,52 @@ SpriteAnimation::AnimationClip* SpriteAnimation::CreateAnimationClipFromFile(con
 		return newClip;
 	}
 
-	/*
+	//csv 파싱 로직
+	std::wifstream file(filePath);
+	if (!file.is_open()) 
+	{
+		wchar_t message[100];
+		swprintf_s(message, _ARRAYSIZE(message), L"%s 경로를 찾을수 없습니다.", filePath);
+		MessageBox(WinGameApp::GetHwnd(), message, L"SpriteAnimation Load Error", MB_OK);
+		return nullptr;
+	}
 	AnimationClip* newClip = new AnimationClip;
-	csv 파싱 로직 들어가야 함.
+	if(newClip == nullptr)
+	{
+		MessageBox(WinGameApp::GetHwnd(), L"메모리 할당 실패", L"SpriteAnimation Load Error", MB_OK);
+		return nullptr; //할당 실패
+	}	
+	std::wstring line;			// 한줄의 문자열
+	int FrameCount = 0;			// 프레임의 개수
+	{
+		std::getline(file, line);		// 첫번째 줄 읽기
+		std::wstringstream wss(line);
+		wss >> FrameCount;
+	}
+	newClip->frames.reserve(FrameCount);
+	for (int j = 0; j < FrameCount; j++)
+	{
+		getline(file, line);		// 한줄 읽기
+		std::wstringstream wss(line);    // 한줄을 읽어서 wstringstream에 저장
+		std::wstring token;
+		{
+			getline(wss, token, L',');	// wss의 내용을 ,를 기준으로 문자열을 분리
+			newClip->frames[j].source.left = (float)_wtoi(token.c_str());
+			getline(wss, token, L',');
+			newClip->frames[j].source.top = (float)_wtoi(token.c_str());
+			getline(wss, token, L',');
+			newClip->frames[j].source.right = (float)_wtoi(token.c_str());
+			getline(wss, token, L',');
+			newClip->frames[j].source.bottom = (float)_wtoi(token.c_str());
+			getline(wss, token, L',');
+			newClip->frames[j].center.x = (float)_wtoi(token.c_str());
+			getline(wss, token, L',');
+			newClip->frames[j].center.y = (float)_wtoi(token.c_str());
+		}
+	}
 	clipResourceMap[filePath] = newClip;
-	*/
-	return nullptr;
+	
+	return newClip;
 }
 
 void SpriteAnimation::ReleaseAnimationClip(const wchar_t* filePath)
