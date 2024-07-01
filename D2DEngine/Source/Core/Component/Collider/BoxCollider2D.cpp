be841b3fc8e42825cd02dad761d7f1243edad4c8
power12562 <kimsiwoo1005@gmail.com>
2024-07-01 21:29:13 +0900

    BoxCollider2D - Debug용 box 그릴때 메트릭스 계산 Render함수에서 하도록 변경

--- D2DEngine/Source/Core/Component/Collider/BoxCollider2D.cpp
+++ D2DEngine/Source/Core/Component/Collider/BoxCollider2D.cpp
@@ -26,14 +26,15 @@ void BoxCollider2D::Start()
 void BoxCollider2D::Update()
 {
 	bounds = gameObject.GetBounds();
-	pivotMatrix = D2D1::Matrix3x2F::Translation(gameObject.transform.pivot.x, gameObject.transform.pivot.y) * gameObject.transform.GetCameraMatrix();
-	trMatrix = D2D1::Matrix3x2F::Translation(pivotMatrix.dx, pivotMatrix.dy); //회전은 적용 안되도록.
+	
 }
 
 void BoxCollider2D::Render()
 {
 	if (isDrawRect)
 	{
+		pivotMatrix = D2D1::Matrix3x2F::Translation(gameObject.transform.pivot.x, gameObject.transform.pivot.y) * gameObject.transform.GetCameraMatrix();
+		trMatrix = D2D1::Matrix3x2F::Translation(pivotMatrix.dx, pivotMatrix.dy); //회전은 적용 안되도록.
 		D2DRenderer::DrawRect(trMatrix, { -bounds.extents.x, -bounds.extents.y, bounds.extents.x, bounds.extents.y}, D2D1::ColorF(D2D1::ColorF::Green));
 	}
 }
