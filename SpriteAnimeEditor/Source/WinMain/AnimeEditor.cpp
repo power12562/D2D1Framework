4f03e3bf8f28245fe180a859727737a832def470
siwoo <power12562@gmail.com>
2024-07-05 08:44:38 +0900

    PenguinBrothers와 Editor 프로젝트 분리

---
+++ SpriteAnimeEditor/Source/WinMain/AnimeEditor.cpp
@@ -0,0 +1,32 @@
+﻿#include "AnimeEditor.h"
+#include "Framework/WorldManager.h"
+
+#include "Source/Scenes/AnimeEditScene.h"
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
+{
+    AnimeEditor app;
+    app.Initialize(hInstance);
+    WorldManager::LoadWorld<AnimeEditScene>();
+    app.Run();
+    app.Uninitialize();
+}
+
+AnimeEditor::AnimeEditor()
+{
+#ifdef _DEBUG
+    isDebug = true;
+#endif // _DEBUG
+    windowName = L"AnimeEditer";
+    winClassName = L"ANI_EDIT";
+    bgColor = D2D1::ColorF(D2D1::ColorF::White);
+
+}
+
+AnimeEditor::~AnimeEditor()
+{
+
+}
---
+++ SpriteAnimeEditor/Source/WinMain/AnimeEditor.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Framework/WinGameApp.h"
+
+class PenguinBrothers : public WinGameApp
+{
+public:
+	PenguinBrothers();
+	virtual ~PenguinBrothers() override;
+
+private:
+
+};
+
