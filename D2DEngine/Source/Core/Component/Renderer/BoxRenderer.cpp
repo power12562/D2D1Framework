56acef517ea9c38df9038db19b7726962c7c9bcc
siwoo <power12562@gmail.com>
2024-09-02 17:00:25 +0900

    BoxRenderer -Render에서 메트릭스 잘못 계산하던 문제 수정

--- D2DEngine/Source/Core/Component/Renderer/BoxRenderer.cpp
+++ D2DEngine/Source/Core/Component/Renderer/BoxRenderer.cpp
@@ -9,7 +9,7 @@ void BoxRenderer::Render()
 {
 	const Vector2 halfSize = size * 0.5f;
 	D2DRenderer::DrawRect(
-		D2D1::Matrix3x2F::Translation(center.x, center.y) * gameObject.transform.GetCameraMatrix(),
+		D2D1::Matrix3x2F::Translation(center.x, -center.y) * gameObject.transform.GetCameraMatrix(),
 		{ -halfSize.x, -halfSize.y, halfSize.x, halfSize.y},
 		color, isFill, alpha
 	);
